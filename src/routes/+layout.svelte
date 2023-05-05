76cb274f17cb56ffd4eda66b70b962ba2516cc54
Ahukko8 <ahukko88@gmail.com>
2023-05-05 07:00:28 +0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,84 @@
+<script>
+	import { Howl, Howler } from "howler";
+	import Segment from "./Segment.svelte";
+	let segments = [
+		{
+			start: 0,
+			end: 3,
+			caption: "Some didnt learn the difficult reality of this path",
+		},
+
+		{
+			start: 4,
+			end: 5,
+			caption: "hey thought Sulh Al-Hudaybiyyah would be their detour",
+		},
+
+		{
+			start: 5.5,
+			end: 9,
+			caption:
+				"and they use it to justify their failure, cowardice, and faltering stances.",
+		},
+
+		{
+			start: 9.2,
+			end: 15,
+			caption: "They use it to open the door of Maslahah for everything",
+		},
+	];
+	const sound = new Howl({
+		src: ["/hudhaybiyya.mp3"],
+	});
+
+	let playAudioButtonText = "Play Audio";
+
+	function playAudio() {
+		if (sound.playing()) {
+			sound.pause();
+			playAudioButtonText = "Play Audio";
+		} else {
+			sound.play();
+			playAudioButtonText = "Pause";
+		}
+	}
+
+	function playSegment(event) {
+		console.log(arguments);
+		sound.seek(event.detail.startTime);
+		sound.play();
+	}
+
+	function addSegment() {
+		const newSegment = {
+			start: 0,
+			end: 0,
+			caption: "",
+		};
+		segments.push(newSegment);
+		segments = segments;
+	}
+</script>
+
+<div class="container">
+	<div>
+		<button type="button" on:click={playAudio} class="btn btn-secondary"
+			>{playAudioButtonText}</button
+		>
+	</div>
+	{#each segments as segment}
+		<Segment
+			caption={segment.caption}
+			on:playaudio={playSegment}
+			startTime={segment.start}
+			endTime={segment.end}
+		/>
+	{/each}
+
+	<div>
+		<hr />
+		<button type="button" on:click={addSegment} class="btn btn-primary">
+			ADDSEGMENT
+		</button>
+	</div>
+</div>
---
+++ src/routes/Segment.svelte
@@ -0,0 +1,74 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    export let caption;
+    export let startTime;
+    export let endTime;
+    function formatTime(seconds) {
+        const milliSeconds = Math.floor(1000 * (seconds - Math.floor(seconds)));
+        let hours = seconds / (60 * 60);
+        hours = Math.floor(hours);
+        let minutes = (seconds % (60 * 60)) / 60;
+        minutes = Math.floor(minutes);
+        const remainingSeconds = seconds % 60;
+        const hoursStr = hours.toString().padStart(2, "0");
+        const minutesStr = minutes.toString().padStart(2, "0");
+        const secondsStr = remainingSeconds
+            .toFixed()
+            .toString()
+            .padStart(2, "0");
+        const milliSecondsStr = milliSeconds
+            .toFixed()
+            .toString()
+            .padStart(3, "0");
+        const time = `${hoursStr}:${minutesStr}:${secondsStr}.${milliSecondsStr}`;
+        return time;
+    }
+    const dispatch = createEventDispatcher();
+
+    function play() {
+        dispatch("playaudio", {
+            startTime: startTime,
+        });
+    }
+</script>
+
+<div class="segment">
+    <div class="row">
+        <div class="col">
+            <span>
+                <button type="button" class="btn btn-success btn-sm">-</button>
+                <span>{formatTime(startTime)}</span>
+                <button type="button" class="btn btn-success btn-sm">+</button>
+                <span>-</span>
+                <button type="button" class="btn btn-success btn-sm">-</button>
+                <span>{formatTime(endTime)}</span>
+                <button type="button" class="btn btn-success btn-sm">+</button>
+            </span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <textarea placeholder="INPUT TEXT" class="form-control"
+                >{caption}</textarea
+            >
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <button type="button" on:click={play} class="btn btn-info"
+                >Play</button
+            >
+        </div>
+    </div>
+</div>
+
+<style>
+    .segment {
+        margin-bottom: 15px;
+        border: 1px solid rgba(0, 0, 0, 0.5);
+        padding: 10px;
+    }
+    .segment:hover {
+        border: 1px solid rgba(0, 0, 0, 0.9);
+    }
+</style>
